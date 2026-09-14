116d96d92241721e26de26a3b3c6396dd73362f2
Terenzzzz <terence592592@gmail.com>
2026-09-14 10:37:24 +0800

    Initial Commit

---
+++ TakeIt/TakeItApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeItApp.swift
+//  TakeIt
+//
+//  Created by 蒋志聪 on 14/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct TakeItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TakeItTests/TakeItTests.swift
@@ -0,0 +1,18 @@
+//
+//  TakeItTests.swift
+//  TakeItTests
+//
+//  Created by 蒋志聪 on 14/09/2026.
+//
+
+import Testing
+
+struct TakeItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
